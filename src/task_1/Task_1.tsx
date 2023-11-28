393b657c13de5e06d341d8327568bc211547580c
Pavel Novik <brn2befree@gmail.com>
2023-11-28 20:44:55 +0300

    finish 3 sprint dop 2

---
+++ src/task_1/Task_1.tsx
@@ -0,0 +1,47 @@
+import {ChangeEvent, FC, RefObject, useCallback, useRef, useState} from 'react';
+
+const CONTAINER_STYLES = {display: 'flex', flexDirection: 'column', gap: 10};
+
+type PropsType = {
+  // value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void
+  // reff: RefObject<HTMLInputElement>
+};
+
+// Task 1
+// If change value on input Title component not re-render
+// If change value on input Task_1 component not re-render
+
+export const Task_1 = () => {
+  // const [value, setValue] = useState('');
+  // const inputValRef = useRef<HTMLInputElement>(null)
+
+  // const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   setValue(e.currentTarget.value);
+  // };
+
+  return (
+    <div style={{...CONTAINER_STYLES} as any}>
+      {/*<Input value={value} onChange={handleChange} />*/}
+      {/*<Input reff={inputValRef} />*/}
+      <Input />
+      <Title title="I am a title" />
+    </div>
+  );
+};
+
+const Title = (props: { title: string }) => <h3>{props.title}</h3>;
+
+const Input: FC<PropsType> = (
+    // {value, onChange}
+    // {reff}
+) => {
+    const [value, setValue] = useState('');
+const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setValue(e.currentTarget.value);
+    };
+  return (
+    // <input type="text" placeholder="Placeholder" value={value} onChange={onChange} />
+    <input type="text" placeholder="Placeholder" value={value} onChange={handleChange} />
+    // <input type="text" ref={reff} />
+  );
+};
